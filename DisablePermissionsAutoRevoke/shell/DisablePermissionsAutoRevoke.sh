8e5b94237323799c7cb53cb23d2c1a03457e74c5
Senliast <LemonDjonites@gmail.com>
2023-05-21 14:14:52 +0200

    Add DisablePermissionsAutoRevoke

---
+++ DisablePermissionsAutoRevoke/shell/DisablePermissionsAutoRevoke.sh
@@ -0,0 +1,8 @@
+appList=$(pm list packages)
+currentAppBeingProcessed=""
+
+while IFS= read -r line;
+do
+	currentAppBeingProcessed=$(echo $line | sed 's/package://g')
+	appops set $currentAppBeingProcessed AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+done <<< "$appList"
