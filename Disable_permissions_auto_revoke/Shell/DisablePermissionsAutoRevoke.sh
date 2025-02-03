e834eb4628ea006c16e1b431996a34b2a9a120f8
Senliast <LemonDjonites@gmail.com>
2025-02-03 19:34:54 +0100

    Structure variables

--- Disable_permissions_auto_revoke/Shell/DisablePermissionsAutoRevoke.sh
+++
@@ -1,8 +0,0 @@
-appList=$(pm list packages)
-currentAppBeingProcessed=""
-
-while IFS= read -r line;
-do
-	currentAppBeingProcessed=$(echo $line | sed 's/package://g')
-	appops set $currentAppBeingProcessed AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
-done <<< "$appList"
