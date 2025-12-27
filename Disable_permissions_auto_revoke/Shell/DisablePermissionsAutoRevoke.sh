79f85317d9b75f4d32031810d6f6330415e6d507
Senliast <LemonDjonites@gmail.com>
2025-12-27 17:39:01 +0100

    [DPAR] Fix for new Android versions
    Make work reliably on latest Android versions by setting permission in uid-mode.

--- Disable_permissions_auto_revoke/Shell/DisablePermissionsAutoRevoke.sh
+++ Disable_permissions_auto_revoke/Shell/DisablePermissionsAutoRevoke.sh
@@ -4,5 +4,5 @@ currentAppBeingProcessed=""
 while IFS= read -r line;
 do
 	currentAppBeingProcessed=$(echo $line | sed 's/package://g')
-	appops set $currentAppBeingProcessed AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+	appops set $currentAppBeingProcessed --uid AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
 done <<< "$appList"
